394e8eb8ad033addaf53a21643493c0c52ca3dea
Mouad <mouadboudhar69@gmail.com>
2026-01-07 19:49:09 +0100

    chore(seed): migrate db changes

--- projet-hopital/Migrations/20260107184845_FixDatabaseSchema.Designer.cs
+++ projet-hopital/Migrations/20260107184845_FixDatabaseSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using projet_hopital.Data;
 
@@ -11,9 +12,11 @@ using projet_hopital.Data;
 namespace projet_hopital.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260107184845_FixDatabaseSchema")]
+    partial class FixDatabaseSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ projet-hopital/Migrations/20260107184845_FixDatabaseSchema.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projet_hopital.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDatabaseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1742436707321-33fed05590bb?q=80&w=851&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1594824476967-48c8b964273f?w=300");
+        }
+    }
+}
